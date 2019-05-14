99d8b63b1c856a7451364db33fde1d598f9f6209
Chennhaoo <38855823+Chennhaoo@users.noreply.github.com>
2019-05-14 22:57:50 +0800

    UP

--- vpstools.sh
+++ vpstools.sh
@@ -1,23 +1,26 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #=================================================
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: VPS Tools
-#	Version: 1.0.0
+#	Version: 1.0.4
 #	Author: ChennHaoo
 #	Blog: https://www.anidays.com
 #=================================================
-sh_ver="1.0.0"
+
+sh_ver="1.0.4"
 filepath=$(cd "$(dirname "$0")"; pwd)
-file_1=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
+file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 BBR_file="${file}/bbr.sh"
 SSH_file="${file}/ssh_port.sh"
-lkl-Haproxy_C_file="${file}/tcp_nanqinlang-haproxy-centos.sh"
-lkl-Haproxy_D_file="${file}/tcp_nanqinlang-haproxy-debian.sh"
-lkl-Rinetd_C_file="${file}/tcp_nanqinlang-rinetd-centos.sh"
-lkl-Rinetd_D_file="${file}/tcp_nanqinlang-rinetd-debianorubuntu.sh"
+SB_file="${file}/superbench.sh"
+UB_file="${file}/unixbench.sh"
+lkl_Haproxy_C_file="${file}/tcp_nanqinlang-haproxy-centos.sh"
+lkl_Haproxy_D_file="${file}/tcp_nanqinlang-haproxy-debian.sh"
+lkl_Rinetd_C_file="${file}/tcp_nanqinlang-rinetd-centos.sh"
+lkl_Rinetd_D_file="${file}/tcp_nanqinlang-rinetd-debianorubuntu.sh"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
@@ -148,16 +151,13 @@ echo -e "${Green_font_prefix} [安装前 请注意] ${Font_color_suffix}
 }
 Install_BBR(){
 	[[ ${release} = "centos" ]] && echo -e "${Error} 本脚本不支持 CentOS系统安装 BBR !" && exit 1
-	echo -e "————————
+	echo -e "
  若使用Debian 9、Ubuntu 18.04之上版本号，可直接选择开启BBR而不需更换内核 
- ———————— 
- 
+———————— 
  1.直接开启
- 2.更换内核开启
- 
-	" && echo
+ 2.更换内核开启" && echo
 	stty erase '^H' && read -p "(默认: 取消):" bbr_ov_1_num
-	[[ -z "${bbr_ov_num}" ]] && echo "已取消..." && exit 1
+	[[ -z "${bbr_ov_1_num}" ]] && echo "已取消..." && exit 1
 	if [[ ${bbr_ov_1_num} == "1" ]]; then
 		BBR_direct
 	elif [[ ${bbr_ov_1_num} == "2" ]]; then
@@ -171,10 +171,12 @@ BBR_direct(){
 	echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 	echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 	sysctl -p
+	echo -e "${Info} BBR 配置完毕"
 	sysctl net.ipv4.tcp_available_congestion_control
-	echo "若显示以下信息，则表示开启成功，请重启服务器后运行检测命令 ${Green_font_prefix}
- sysctl net.ipv4.tcp_available_congestion_control
- net.ipv4.tcp_available_congestion_control = bbr cubic reno${Font_color_suffix}
+	echo -e "
+ 若上方显示以下信息，则表示开启成功，请重启服务器后运行检测命令 
+ ${Green_font_prefix}sysctl net.ipv4.tcp_available_congestion_control${Font_color_suffix}
+ ${Green_font_prefix}net.ipv4.tcp_available_congestion_control = bbr cubic reno${Font_color_suffix}
 	" && echo
 }
 Start_BBR(){
@@ -204,21 +206,26 @@ BBR_installation_status(){
 
 #OpenVZ BBR
 Configure_BBR_OV(){
+	echo -e "${Info} "
 	cat /dev/net/tun
-	echo "若显示 ${Green_font_prefix} cat: /dev/net/tun: File descriptor in bad state ${Font_color_suffix}，则表示你的VPS支持此脚本，否则请发工单开启${Green_font_prefix} TUN/TAP支持 ${Font_color_suffix} " && echo
-	echo "若VPS支持请继续 [y/N]" && echo
+	echo -e "————————"
+	echo -e "若显示${Green_font_prefix} cat: /dev/net/tun: File descriptor in bad state ${Font_color_suffix}，则表示你的VPS支持此脚本，否则请发工单开启${Green_font_prefix} TUN/TAP 支持${Font_color_suffix}
+	"
+	read -e -p "若VPS支持请继续 [y/N]（默认取消）：" unyn
+	[[ -z "${unyn}" ]] && echo "已取消..." && exit 1
 	if [[ ${unyn} == [Nn] ]]; then
 		echo && echo -e "${Info} 已取消..." && exit 1
 		else
-		echo -e "————————
- ${Green_font_prefix} [lkl-haproxy] ${Font_color_suffix} 与  ${Green_font_prefix} [lkl-rinetd] ${Font_color_suffix} 只能二选一，两者同时安装后果自负！！
+		echo -e "
+ ${Green_font_prefix}[lkl-haproxy] ${Font_color_suffix}与${Green_font_prefix} [lkl-rinetd] ${Font_color_suffix}只能二选一，两者同时安装后果自负！！
  本脚本来自于南琴浪（https://github.com/tcp-nanqinlang/wiki/wiki），使用本脚本带有一定风险，请做好数据备份！ 
- ————————
- 
+————————
+
  1.Lkl-Haproxy（建议）
- 2.Lkl-Rinetd 
  
- ———————— 
+ 2.Lkl-Rinetd
+ 
+———————— 
  Lkl-Haproxy
    端口修改文件：/home/tcp_nanqinlang
    支持：单端口、端口段
@@ -226,8 +233,7 @@ Configure_BBR_OV(){
   
  Lkl-Rinetd
    端口修改文件：/home/tcp_nanqinlang
-   支持：单端口，多个端口号用空格隔开 
-		" && echo
+   支持：单端口，多个端口号用空格隔开 " && echo
 		stty erase '^H' && read -p "(默认: 取消):" bbr_ov_num
 		[[ -z "${bbr_ov_num}" ]] && echo "已取消..." && exit 1
 		if [[ ${bbr_ov_num} == "1" ]]; then
@@ -243,57 +249,57 @@ Configure_BBR_OV(){
 #OpenVZ BBR lkl-Haproxy
 Lkl-Haproxy(){
 	if [[ ${release} == "centos" ]]; then
-		if [[ ! -e ${lkl-Haproxy_C_file} ]]; then
-			echo -e "${Error} 没有发现 Lkl-Haproxy 脚本，开始下载..."
+		if [[ ! -e ${lkl_Haproxy_C_file} ]]; then
+			echo -e "${Error} 没有发现 Lkl-Haproxy for CentOS 脚本，开始下载..."
 			cd "${file}"
 			if ! wget -N --no-check-certificate https://github.com/Chennhaoo/Shell_Bash/raw/master/other/lkl-haproxy/tcp_nanqinlang-haproxy-centos.sh; then
-				echo -e "${Error} Lkl-Haproxy 脚本下载失败 !" && exit 1
+				echo -e "${Error} Lkl-Haproxy for CentOS 脚本下载失败 !" && exit 1
 			else
-				echo -e "${Info} Lkl-Haproxy 脚本下载完成 !"
+				echo -e "${Info} Lkl-Haproxy for CentOS 脚本下载完成 !"
 				chmod +x tcp_nanqinlang-haproxy-centos.sh
 			fi
 		fi
-		bash "${lkl-Haproxy_C_file}"
+		bash "${lkl_Haproxy_C_file}"
 	else
-		if [[ ! -e ${lkl-Haproxy_D_file} ]]; then
-			echo -e "${Error} 没有发现 Lkl-Haproxy 脚本，开始下载..."
+		if [[ ! -e ${lkl_Haproxy_D_file} ]]; then
+			echo -e "${Error} 没有发现 Lkl-Haproxy for Debian 脚本，开始下载..."
 			cd "${file}"
 			if ! wget -N --no-check-certificate https://github.com/Chennhaoo/Shell_Bash/raw/master/other/lkl-haproxy/tcp_nanqinlang-haproxy-debian.sh; then
-				echo -e "${Error} Lkl-Haproxy 脚本下载失败 !" && exit 1
+				echo -e "${Error} Lkl-Haproxy for Debian 脚本下载失败 !" && exit 1
 			else
-				echo -e "${Info} Lkl-Haproxy 脚本下载完成 !"
+				echo -e "${Info} Lkl-Haproxy for Debian 脚本下载完成 !"
 				chmod +x tcp_nanqinlang-haproxy-debian.sh
 			fi
 		fi
-		bash "${lkl-Haproxy_D_file}"
+		bash "${lkl_Haproxy_D_file}"
 	fi
 }
 #OpenVZ BBR lkl-Rinetd
 Lkl-Rinetd(){
 	if [[ ${release} == "centos" ]]; then
-		if [[ ! -e ${lkl-Rinetd_C_file} ]]; then
-			echo -e "${Error} 没有发现 Lkl-Rinetd 脚本，开始下载..."
+		if [[ ! -e ${lkl_Rinetd_C_file} ]]; then
+			echo -e "${Error} 没有发现 Lkl-Rinetd for CentOS 脚本，开始下载..."
 			cd "${file}"
 			if ! wget -N --no-check-certificate https://github.com/Chennhaoo/Shell_Bash/raw/master/other/lkl-rinetd/tcp_nanqinlang-rinetd-centos.sh; then
-				echo -e "${Error} Lkl-Rinetd 脚本下载失败 !" && exit 1
+				echo -e "${Error} Lkl-Rinetd for CentOS 脚本下载失败 !" && exit 1
 			else
-				echo -e "${Info} Lkl-Rinetd 脚本下载完成 !"
+				echo -e "${Info} Lkl-Rinetd for CentOS 脚本下载完成 !"
 				chmod +x tcp_nanqinlang-rinetd-centos.sh
 			fi
 		fi
-		bash "${lkl-Rinetd_C_file}"
+		bash "${lkl_Rinetd_C_file}"
 	else
-		if [[ ! -e ${lkl-Rinetd_D_file} ]]; then
-			echo -e "${Error} 没有发现 Lkl-Rinetd 脚本，开始下载..."
+		if [[ ! -e ${lkl_Rinetd_D_file} ]]; then
+			echo -e "${Error} 没有发现 Lkl-Rinetd for Debian 脚本，开始下载..."
 			cd "${file}"
 			if ! wget -N --no-check-certificate https://raw.githubusercontent.com/Chennhaoo/Shell_Bash/master/other/lkl-rinetd/tcp_nanqinlang-rinetd-debianorubuntu.sh; then
-				echo -e "${Error} Lkl-Rinetd 脚本下载失败 !" && exit 1
+				echo -e "${Error} Lkl-Rinetd for Debian 脚本下载失败 !" && exit 1
 			else
-				echo -e "${Info} Lkl-Rinetd 脚本下载完成 !"
+				echo -e "${Info} Lkl-Rinetd for Debian 脚本下载完成 !"
 				chmod +x tcp_nanqinlang-rinetd-debianorubuntu.sh
 			fi
 		fi
-		bash "${lkl-Rinetd_D_file}"
+		bash "${lkl_Rinetd_D_file}"
 	fi
 }
 
@@ -372,58 +378,108 @@ UnBanBTPTSPAM(){
 	rm -rf ban_iptables.sh
 }
 
+#SuperBench测试
+Install_SB(){
+	if [[ ! -e ${SB_file} ]]; then
+		echo -e "${Error} 没有发现 SuperBench 测试脚本，开始下载..."
+		cd "${file}"
+		if ! wget -N --no-check-certificate https://raw.githubusercontent.com/oooldking/script/master/superbench.sh; then
+			echo -e "${Error} SuperBench 测试脚本下载失败 !" && exit 1
+		else
+			echo -e "${Info} SuperBench 测试脚本下载完成 !"
+			chmod +x superbench.sh
+		fi
+	fi
+	bash "${SB_file}"
+}
 
-echo && echo -e " VPS工具包 一键安装管理脚本 ${Red_font_prefix}V 1.0.0 19-5-12${Font_color_suffix}
+#UnixBench测试
+Install_UB(){
+	if [[ ! -e ${UB_file} ]]; then
+		echo -e "${Error} 没有发现 UnixBench 测试脚本，开始下载..."
+		cd "${file}"
+		if ! wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/unixbench.sh; then
+			echo -e "${Error} UnixBench 测试脚本下载失败 !" && exit 1
+		else
+			echo -e "${Info} UnixBench 测试脚本下载完成 !"
+			chmod +x unixbench.sh
+		fi
+	fi
+	echo "确定开始 UnixBench 测试吗 ？[y/N]" && echo
+	stty erase '^H' && read -p "(默认: y):" unyn 
+	if [[ ${unyn} == [Nn] ]]; then
+		echo && echo -e "${Info} 已取消..." && exit 1
+		else
+			if [[ ${release} == "centos" ]]; then
+				yum install libc6-dev -y
+			else
+				apt-get install libc6-dev -y
+			fi		
+		bash "${UB_file}"
+	fi
+}
+
+
+
+
+
+echo -e " VPS工具包 一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   -- Toyo | ChennHaoo --
   
-  
- ${Green_font_prefix} 0.${Font_color_suffix} 安装常用依赖
- ${Green_font_prefix} 1.${Font_color_suffix} 更新软件源
- ${Green_font_prefix} 2.${Font_color_suffix} 更新系统（慎重）
- ${Green_font_prefix} 3.${Font_color_suffix} 修改系统时间
+ ${Green_font_prefix} 1.${Font_color_suffix} 安装常用依赖
+ ${Green_font_prefix} 2.${Font_color_suffix} 更新软件源
+ ${Green_font_prefix} 3.${Font_color_suffix} 更新系统（慎重）
+ ${Green_font_prefix} 4.${Font_color_suffix} 修改系统时间
 ————————————
- ${Green_font_prefix} 4.${Font_color_suffix} 修改 SSH端口（宝塔用户请勿使用）
- ${Green_font_prefix} 5.${Font_color_suffix} 配置 KVM BBR
- ${Green_font_prefix} 6.${Font_color_suffix} 配置 OpenVZ BBR
- ${Green_font_prefix} 7.${Font_color_suffix} 修改系统文件句柄数（慎用）
- ${Green_font_prefix} 8.${Font_color_suffix} 一键封禁 BT/PT/SPAM (iptables)
- ${Green_font_prefix} 9.${Font_color_suffix} 一键解封 BT/PT/SPAM (iptables)
-————————————" && echo
-echo
-read -e -p " 请输入数字 [1-9]:" num
+ ${Green_font_prefix} 5.${Font_color_suffix} 修改 SSH端口（宝塔用户请勿使用）
+ ${Green_font_prefix} 6.${Font_color_suffix} 配置 KVM BBR
+ ${Green_font_prefix} 7.${Font_color_suffix} 配置 OpenVZ BBR
+ ${Green_font_prefix} 8.${Font_color_suffix} 修改系统文件句柄数（慎用）
+ ${Green_font_prefix} 9.${Font_color_suffix} 一键封禁 BT/PT/SPAM (iptables)
+ ${Green_font_prefix} 10.${Font_color_suffix} 一键解封 BT/PT/SPAM (iptables)
+————————————
+ ${Green_font_prefix} 11.${Font_color_suffix} SuperBench 测试
+ ${Green_font_prefix} 12.${Font_color_suffix} UnixBench 测试（时间较长）
+" && echo
+read -e -p " 请输入数字 [1-12]:" num
 case "$num" in
-	0)
+	1)
 	SYS_Tools
 	;;
-	1)
+	2)
 	Update_SYS_Y
 	;;
-	2)
+	3)
 	Update_SYS
 	;;
-	3)
+	4)
 	SYS_Time
 	;;
-	4)
+	5)
 	Install_SSHPor
 	;;
-	5)
+	6)
 	Configure_BBR
 	;;
-	6)
+	7)
 	Configure_BBR_OV
 	;;
-	7)
+	8)
 	SYS_limits
 	;;
-	8)
+	9)
 	BanBTPTSPAM
 	;;
-	9)
+	10)
 	UnBanBTPTSPAM
 	;;
+	11)
+	Install_SB
+	;;
+	12)
+	Install_UB
+	;;
 	*)
-	echo "请输入正确数字 [1-9]"
+	echo "请输入正确数字 [1-12]"
 	;;
 esac
-fi
